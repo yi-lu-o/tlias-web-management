006335b7cc02941b71ece7b52bf3f8bc84b9c272
雷一路 <11492284+lei-yilu@user.noreply.gitee.com>
2023-10-20 10:54:50 +0800

    一个普通本科生学习B站教程做的练手小项目

---
+++ src/test/java/com/YiLuLearner/TliasWebManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.YiLuLearner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TliasWebManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
